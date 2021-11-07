2b56152133a0c0befd7cba6330e5642c4a53f3a3
desw012 <desw012@gmail.com>
2021-11-07 11:10:33 +0900

    test

---
+++ user/src/test/java/com/pdi/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdi.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
