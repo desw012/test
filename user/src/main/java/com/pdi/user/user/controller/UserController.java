9c8bd939fafe4f31ad1d8d442acf30eee2a2e83f
desw012 <desw012@gmail.com>
2021-11-07 11:09:13 +0900

    test

---
+++ user/src/main/java/com/pdi/user/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.pdi.user.user.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    public String sign(){
+        return null;
+    }
+}
